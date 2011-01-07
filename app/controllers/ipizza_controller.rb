173dfb0d21eea6fd8fbc8edf28548c123800f4e1
Priit Haamer <priit@fraktal.ee>
2011-01-07 12:17:49 +0200

    Providing more information in user interface and e-mail messages.

--- app/controllers/ipizza_controller.rb
+++ app/controllers/ipizza_controller.rb
@@ -7,18 +7,18 @@ class IpizzaController < ApplicationController
     bank_response = provider.payment_response(params)
     
     bank_message = BankMessage.create_from_params!(params, bank_response)
-    payment = Payment.find(bank_message.payment_id)
+    @payment = Payment.find(bank_message.payment_id)
     
     if bank_response.valid? and bank_response.success?
-      if payment and not payment.delivered?
-        payment.deliver!
-        PaymentResponseMailer.payment_received(bank_message, payment).deliver
-        PaymentResponseMailer.payment_confirmation(bank_message, payment).deliver
+      if @payment and not @payment.delivered?
+        @payment.deliver!
+        PaymentResponseMailer.payment_received(bank_message, @payment).deliver
+        PaymentResponseMailer.payment_confirmation(bank_message, @payment).deliver
       end  
       render :action => :success
     else
-      if payment and not payment.cancelled?
-        payment.cancel!
+      if @payment and not @payment.cancelled?
+        @payment.cancel!
       end
       render :action => :cancel
     end
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,18 +3,18 @@ class PaymentsController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
   def create
-    pmt = Payment.create!(
+    @payment = Payment.create!(
       :order_id => params[:ord], :description => params[:des], :amount => params[:amt],
       :frequency => params[:frq], :customer_email => params[:ceml], :return_path => params[:ret],
       :opt => params[:opt], :currency => 'EUR'
     )
     
-    @payment = Ipizza::Payment.new(
-      :stamp => pmt.id, :amount => pmt.amount, :refnum => pmt.order_id,
-      :message => pmt.description, :currency => pmt.currency
+    ipizza_payment = Ipizza::Payment.new(
+      :stamp => @payment.id, :amount => @payment.amount, :refnum => @payment.order_id,
+      :message => @payment.description, :currency => @payment.currency
     )
     
-    @swed_request = Ipizza::Provider::Swedbank.new.payment_request(@payment)
-    @seb_request = Ipizza::Provider::Seb.new.payment_request(@payment)
+    @swed_request = Ipizza::Provider::Swedbank.new.payment_request(ipizza_payment)
+    @seb_request = Ipizza::Provider::Seb.new.payment_request(ipizza_payment)
   end
 end
