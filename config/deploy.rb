f9ecb239cea58bde736a991272965f905c2b15f2
Tanel Jakobsoo <tanel.jakobsoo@gmail.com>
2015-02-26 23:20:43 +0200

    Improved example deploy script
    Enable gem binaries generation on deploy to correct place. Otherwise no binaries (e.g command "rails c production") are available since they are linked.

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,6 +29,9 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
 
+# Generate gem binaries on every deploy since ./bin folder is linked.
+set :bundle_binstubs, -> { shared_path.join('bin') }
+
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
